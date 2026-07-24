c623b3342d1330118bfc27332d9520b5f8618fc6
aradhana <aradhanashivhare374@gmail.com>
2026-07-24 15:56:21 +0530

    changes in Service

--- backend/routes/Service.js
+++ backend/routes/Service.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 const {
   getServices,
-} = require("../controllers/serviceController");
+} = require("../controllers/ServiceController");
 
 router.get("/", getServices);
 
